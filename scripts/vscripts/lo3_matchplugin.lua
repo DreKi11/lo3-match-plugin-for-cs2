b8690544f7459d84053e178eda26d4437185b9af
DreKill <151403396+DreKi11@users.noreply.github.com>
2023-11-20 16:00:38 +0100

    Update lo3_matchplugin.lua
    Mainly modified in 3 clearly distinct phases :)

--- scripts/vscripts/lo3_matchplugin.lua
+++ scripts/vscripts/lo3_matchplugin.lua
@@ -1,10 +1,50 @@
-require "libs.timers"
-chatPrefix = "{darkgreen} [LO3] "
+--[[
+******************************************************[ lo3 gsys matchplugin CS2 ]********************************************************
+	Original plugin created by: execut1ve
+	Version: 1.0
+	Github: https://github.com/execut1ve/
+	Twitter: https://twitter.com/execut1ve
+	Steam: https://steamcommunity.com/id/execut1ve/
 
+	Improved by: DreKill
+	Version: 1.0
+	Github: https://github.com/DreKi11/
+	Steam: https://steamcommunity.com/profiles/76561197965623687
+
+	*<News>
+	  Added PrintWaitingPracc (Print message during the pracc period) [.restart .forceend(back to warmup)]					- V
+	  Added PrintWaitingPug (Print message during the match period) [.pause .unpause .restart(restart score) .forceend]		- V
+	* PrintWaitingforPlayers *(default print message during the warmup period) [.lo3 .pracc .scramble .help]				- V
+	- Added timers WARPUG_TIME, WARPRACC_TIME																				- V
+	* Use timer WARMUP_TIME *(default timer and config)																		- V
+	- Added .pracc, .restart <Convars:GetCommandClient()>																	- V
+	- Replace <fr> traduction																								- V
+	*<Past>
+	_____________________________________________________________________________________________________________________________
+	
+	USABILITY:
+	- Ce plugin est un simple lo3 5vs5 competitive pour votre serveur
+	- Warmup time par défaut, switch pour lo3 ou pracc configuration
+	- 3 Phases (Warmup time/Match time/Pracc time)
+************************************************************************************************************************************
+--]]
+
+require "libs.timers"
+
+chatPrefix = "{darkgreen} [LO3] "
+ 
+-- Les valeurs ci-dessous sont à réglées dans les timers à partir de la ligne <'95'>
+-- Affiche des messages à intervalle régulier pendant les phases de || warmup || lo3 || pracc ||
+-- -------------------------------------------------------------------
 WARMUP_TIME = true
-warmupDelay = 0
+warmupDelay = 0 -- warmupDelay 45 par default pour les messages pendant le warmup (à ne pas régler ici)
+WARPUG_TIME = false
+warpugDelay = 0 -- warpugDelay 90 par default pour les messages pendant le lo3 (à ne pas régler ici)
+WARPRACC_TIME = false
+warpraccDelay = 0 -- warpraccDelay 60 par default pour les messages pendant le pracc (à ne pas régler ici)
 scrambleDelay = 0
 scrambleTime = 0
+-- -------------------------------------------------------------------
 
 function HC_ReplaceColorCodes_pug(text)
 	text = string.gsub(text, "{white}", "\x01")
@@ -32,27 +72,32 @@ end
 
 function PrintHelp()
     HC_PrintChatAll_pug("{white}------------------------------------------------")
-    HC_PrintChatAll_pug("{white}【ウォームアップ中】")
-    HC_PrintChatAll_pug("{red}.lo3{white} : 試合を開始します。")
-    HC_PrintChatAll_pug("{red}.restart{white} : ウォームアップをリスタートします。")
-    HC_PrintChatAll_pug("{red}.scramble{white} : チームメンバーを3回シャッフルします。")
-    HC_PrintChatAll_pug("{white}【試合中】")
-    HC_PrintChatAll_pug("{red}.pause{white} : 試合をポーズ状態にします。")
-    HC_PrintChatAll_pug("{red}.unpause{white} : 試合のポーズ状態を解除します。")
-    HC_PrintChatAll_pug("{red}.forceend{white} : 試合を強制終了します。")
+    HC_PrintChatAll_pug("{white}【Échauffement】")
+    HC_PrintChatAll_pug("{darkgreen}.lo3{white} : Commence le match。")
+    HC_PrintChatAll_pug("{darkgreen}.pracc{white} : Commence le pracc。")
+    HC_PrintChatAll_pug("{darkgreen}.cut{white} : Commence le cut round。")
+    HC_PrintChatAll_pug("{darkgreen}.scramble{white} : Mélange les équipes 3 fois。")
+    HC_PrintChatAll_pug("{white}【Pendant la partie】")
+    HC_PrintChatAll_pug("{red}.pause{white} : Demande une pause au prochain freezetime。")
+    HC_PrintChatAll_pug("{red}.unpause{white} : Annule la pause demandée。")
+    HC_PrintChatAll_pug("{red}.restart{white} : Restart le match、 les scores reviennent à 0。")
+    HC_PrintChatAll_pug("{red}.forceend{white} : Force la fin du match/pracc。")
     HC_PrintChatAll_pug("{white}------------------------------------------------")
 end
 
+-- Timer pendant le warmup pour afficher toutes les x secondes les commandes
 function PrintWaitingforPlayers(event)
     Timers:CreateTimer("warmup_timer", {
         callback = function()
             if (WARMUP_TIME == true) then
-                if warmupDelay == 20 then
+                if warmupDelay == 45 then
                     SendToServerConsole("bot_kick")
-                    HC_PrintChatAll_pug("{white} ウォームアップ中、sv_cheatsは有効です。")
-                    HC_PrintChatAll_pug("{white} 全員の準備が確認できたあと、")
-                    HC_PrintChatAll_pug("{white} コンソールに {red}.lo3{white} とタイプして試合を開始します。")
-                    HC_PrintChatAll_pug("{white} コンソールに {red}.help{white} とタイプしてヘルプを表示します。")
+                    HC_PrintChatAll_pug("{white} Warmup en cours...。")
+                    HC_PrintChatAll_pug("{white} à la console tape {darkgreen}.lo3{white} Commence le match。")
+                    HC_PrintChatAll_pug("{white} à la console tape {darkgreen}.pracc{white} Commence le pracc。")
+                    HC_PrintChatAll_pug("{white} à la console tape {darkgreen}.cut{white} Commence le cut round。")
+					HC_PrintChatAll_pug("{white} à la console tape {darkgreen}.scramble{white} Mélange les équipes 3 fois。")
+                    HC_PrintChatAll_pug("{white} à la console tape {orange}.help{white} Obtenir de l'aide。")
                     warmupDelay = 0
                 else
                     warmupDelay = warmupDelay + 1
@@ -63,50 +108,160 @@ function PrintWaitingforPlayers(event)
     })
 end
 
+-- Timer pendant le pug pour afficher toutes les x secondes les commandes
+function PrintWaitingPug(event)
+    Timers:CreateTimer("warpug_timer", {
+        callback = function()
+            if (WARPUG_TIME == true) then
+                if warpugDelay == 90 then
+                    SendToServerConsole("bot_kick")
+                    HC_PrintChatAll_pug("{white} Match en cours...。")
+                    HC_PrintChatAll_pug("{white} à la console tape {red}.pause{white} Demande une pause au prochain freezetime。")
+                    HC_PrintChatAll_pug("{white} à la console tape {red}.unpause{white} Annule la pause demandée。")
+					HC_PrintChatAll_pug("{white} à la console tape {red}.restart{white} Restart le match、 les scores reviennent à 0。")
+					HC_PrintChatAll_pug("{white} à la console tape {red}.forceend{white} Force la fin du match。")
+                    warpugDelay = 0
+                else
+                    warpugDelay = warpugDelay + 1
+                end
+            end
+            return 1
+        end,
+    })
+end
+
+-- Timer pendant le pracc pour afficher toutes les x secondes les commandes
+function PrintWaitingPracc(event)
+    Timers:CreateTimer("warpracc_timer", {
+        callback = function()
+            if (WARPRACC_TIME == true) then
+                if warpraccDelay == 60 then
+                    HC_PrintChatAll_pug("{white} Pracc en cours...。")
+					HC_PrintChatAll_pug("{white} à la console tape {red}.restart{white} Restart le pracc、 les scores reviennent à 0。")
+					HC_PrintChatAll_pug("{white} à la console tape {red}.forceend{white} Relance le warmup。")
+                    warpraccDelay = 0
+                else
+                    warpraccDelay = warpraccDelay + 1
+                end
+            end
+            return 1
+        end,
+    })
+end
+
+-- Phase de warmup (par défaut)
 function StartWarmup()
     SendToServerConsole("mp_unpause_match")
     SendToServerConsole("bot_kick")
+    SendToServerConsole("bot_quota 0")
+    SendToServerConsole("bot_quota_mode fill")
+	SendToServerConsole("mp_buytime 9999")
+	SendToServerConsole("mp_startmoney 800")
+	SendToServerConsole("mp_maxmoney 6000")
+	SendToServerConsole("mp_roundtime 0")
+	SendToServerConsole("sv_allow_votes 0")
+	SendToServerConsole("mp_endmatch_votenextleveltime 0")
+	SendToServerConsole("mp_endmatch_votenextmap 0")
+	SendToServerConsole("mp_endmatch_votenextmap_keepcurrent 0")
     SendToServerConsole("sv_cheats 1")
-    SendToServerConsole("sv_grenade_trajectory_prac_pipreview 1")
-    SendToServerConsole("sv_grenade_trajectory_prac_trailtime 4")
+    SendToServerConsole("mp_respawn_on_death_t 0")
+    SendToServerConsole("mp_respawn_on_death_ct 0")
+    SendToServerConsole("mp_autokick 0")
+    SendToServerConsole("sv_grenade_trajectory_prac_pipreview 0")
+    SendToServerConsole("sv_grenade_trajectory_prac_trailtime 0")
+    SendToServerConsole("sv_showimpacts 0")
+    SendToServerConsole("sv_showimpacts_time 0")
     SendToServerConsole("sv_infinite_ammo 2")
     SendToServerConsole("mp_buy_anywhere 1")
     SendToServerConsole("mp_warmuptime 234124235")
     SendToServerConsole("mp_warmuptime_all_players_connected 234124235")
     SendToServerConsole("mp_warmup_pausetimer 1")
 	SendToServerConsole("mp_warmup_start")
-    HC_PrintChatAll_pug("{white} ウォームアップ中、sv_cheatsは有効です。")
-    HC_PrintChatAll_pug("{white} 全員の準備が確認できたあと、")
-    HC_PrintChatAll_pug("{white} コンソールに {red}.lo3{white} とタイプして試合を開始します。")
-    HC_PrintChatAll_pug("{white} コンソールに {red}.help{white} とタイプしてヘルプを表示します。")
+	SendToServerConsole("nextmap_print_enabled 1")
     WARMUP_TIME = true
+	WARPUG_TIME = false
+	WARPRACC_TIME = false
 end
 
+-- Phase de match
 function StartPug()
     SendToServerConsole("bot_kick")
+    SendToServerConsole("bot_quota 0")
+    SendToServerConsole("bot_quota_mode fill")
+	SendToServerConsole("mp_buytime 20")
+	SendToServerConsole("mp_startmoney 800")
+	SendToServerConsole("mp_maxmoney 16000")
+	SendToServerConsole("mp_roundtime 1.92")
+	SendToServerConsole("sv_allow_votes 1")
+	SendToServerConsole("mp_endmatch_votenextleveltime 2")
+	SendToServerConsole("mp_endmatch_votenextmap 1")
+	SendToServerConsole("mp_endmatch_votenextmap_keepcurrent 1")
     SendToServerConsole("mp_warmup_start")
     SendToServerConsole("sv_grenade_trajectory_prac_pipreview 0")
     SendToServerConsole("sv_grenade_trajectory_prac_trailtime 0")
+    SendToServerConsole("sv_showimpacts 0")
+    SendToServerConsole("sv_showimpacts_time 0")
     SendToServerConsole("sv_infinite_ammo 0")
     SendToServerConsole("mp_buy_anywhere 0")
     SendToServerConsole("sv_cheats 0")
+    SendToServerConsole("mp_respawn_on_death_t 0")
+    SendToServerConsole("mp_respawn_on_death_ct 0")
+    SendToServerConsole("mp_autokick 0")
     SendToServerConsole("mp_warmuptime 10")
     SendToServerConsole("mp_warmuptime_all_players_connected 10")
     SendToServerConsole("mp_warmup_pausetimer 0")
-    HC_PrintChatAll_pug("{white} 10秒後に試合が開始されます。")
-    HC_PrintChatAll_pug("{white} 10秒後に試合が開始されます。")
-    HC_PrintChatAll_pug("{white} 10秒後に試合が開始されます。")
+	SendToServerConsole("nextmap_print_enabled 1")
+    HC_PrintChatAll_pug("{white} Le match commencera dans 10 secondes。")
+    HC_PrintChatAll_pug("{white} Le match commencera dans 10 secondes。")
+    HC_PrintChatAll_pug("{white} Le match commencera dans 10 secondes。")
     WARMUP_TIME = false
-end	
+	WARPUG_TIME = true
+	WARPRACC_TIME = false
+end
+
+-- Phase de pracc
+function StartPracc()
+    SendToServerConsole("bot_quota 10")
+    SendToServerConsole("bot_quota_mode fill")
+	SendToServerConsole("mp_buytime 9999")
+	SendToServerConsole("mp_startmoney 800")
+	SendToServerConsole("mp_maxmoney 12000")
+	SendToServerConsole("mp_roundtime 60")
+	SendToServerConsole("sv_allow_votes 1")
+	SendToServerConsole("mp_endmatch_votenextleveltime 2")
+	SendToServerConsole("mp_endmatch_votenextmap 1")
+	SendToServerConsole("mp_endmatch_votenextmap_keepcurrent 1")
+    SendToServerConsole("mp_warmup_start")
+    SendToServerConsole("sv_grenade_trajectory_prac_pipreview 1")
+    SendToServerConsole("sv_grenade_trajectory_prac_trailtime 5")
+    SendToServerConsole("sv_showimpacts 1")
+    SendToServerConsole("sv_showimpacts_time 1")
+    SendToServerConsole("sv_infinite_ammo 2")
+    SendToServerConsole("mp_buy_anywhere 1")
+    SendToServerConsole("sv_cheats 1")
+    SendToServerConsole("mp_respawn_on_death_t 0")
+    SendToServerConsole("mp_respawn_on_death_ct 0")
+    SendToServerConsole("mp_autokick 0")
+    SendToServerConsole("mp_warmuptime 10")
+    SendToServerConsole("mp_warmuptime_all_players_connected 10")
+    SendToServerConsole("mp_warmup_pausetimer 0")
+	SendToServerConsole("nextmap_print_enabled 1")
+    HC_PrintChatAll_pug("{white} Le pracc commence。")
+    HC_PrintChatAll_pug("{white} Le pracc commence。")
+    HC_PrintChatAll_pug("{white} Le pracc commence。")
+    WARMUP_TIME = false
+	WARPUG_TIME = false
+	WARPRACC_TIME = true
+end
 
 function ScrambleTeams()
     scrambleTime = 2
-    HC_PrintChatAll_pug("{white} チームメンバーがあと3回シャッフルされます。")
+    HC_PrintChatAll_pug("{white} Les membres de l'équipe seront mélangés 3 fois de plus。")
     SendToServerConsole("mp_scrambleteams")
     Timers:CreateTimer("scramble_timer", {
         callback = function()
             if scrambleDelay == 1 then
-                HC_PrintChatAll_pug("{white} チームメンバーがあと"..scrambleTime.."回シャッフルされます。")
+                HC_PrintChatAll_pug("{white} Les membres de l'équipe sont partis "..scrambleTime.." sera mélangé deux fois。")
                 SendToServerConsole("mp_scrambleteams")
                 scrambleTime = scrambleTime - 1
                 scrambleDelay = 0
@@ -121,6 +276,18 @@ function ScrambleTeams()
     })
 end
 
+--[[
+Déclaration des commandes possible pour les utilisateurs selon la configuration
+.lo3
+.pracc
+.help
+.pause
+.unpause
+.restart
+.scramble
+.forceend
+--]]
+
 Convars:RegisterCommand(".lo3", function() 
 	local user = Convars:GetCommandClient()
 	
@@ -129,46 +296,78 @@ Convars:RegisterCommand(".lo3", function()
 	end
 end, nil, 0)
 
+Convars:RegisterCommand(".pracc", function() 
+	local user = Convars:GetCommandClient()
+	
+	if (WARMUP_TIME == true) then
+		StartPracc()
+	end
+end, nil, 0)
+
 Convars:RegisterCommand(".help", function() 
 	local user = Convars:GetCommandClient()
 	PrintHelp()
 end, nil, 0)
 
-Convars:RegisterCommand(".forceend", function()
+Convars:RegisterCommand(".pause", function()
+	local user = Convars:GetCommandClient()
+    SendToServerConsole("mp_pause_match")
+    HC_PrintChatAll_pug("{white} Qqun demande une pause au prochain freezetime。")
+end, nil, 0)
+
+Convars:RegisterCommand(".unpause", function()
+	local user = Convars:GetCommandClient()
+    SendToServerConsole("mp_unpause_match")
+    HC_PrintChatAll_pug("{white} La pause demandée vient d'être annulée/stoppée。")
+end, nil, 0)
+
+Convars:RegisterCommand(".restart", function()
 	local user = Convars:GetCommandClient()
     if (WARMUP_TIME == false) then
-        HC_PrintChatAll_pug("{red} 試合が中止されました。")
-        StartWarmup()
+	    SendToServerConsole("mp_restartgame 3")
+        HC_PrintChatAll_pug("{white} Restart du match/pracc、 les scores reviennent donc à 0。")
     end
 end, nil, 0)
 
 Convars:RegisterCommand(".scramble", function()
 	local user = Convars:GetCommandClient()
-
 	if (WARMUP_TIME == true) then
+        HC_PrintChatAll_pug("{white} Mélange des équipes。")
         ScrambleTeams()
     end
 end, nil, 0)
 
-Convars:RegisterCommand(".pause", function()
+Convars:RegisterCommand(".forceend", function()
 	local user = Convars:GetCommandClient()
-	
-    SendToServerConsole("mp_pause_match")
-    HC_PrintChatAll_pug("{white} 試合がポーズ状態になります。")
-end, nil, 0)
-
-Convars:RegisterCommand(".unpause", function()
-	local user = Convars:GetCommandClient()
-    HC_PrintChatAll_pug("{white} 試合のポーズ状態が解除されます。")
-    SendToServerConsole("mp_unpause_match")
+    if (WARMUP_TIME == false) then
+        HC_PrintChatAll_pug("{white} Le match/pracc a été annulé。")
+        StartWarmup()
+    end
 end, nil, 0)
 
 function OnPlayerSpawned(event)
 	PrintWaitingforPlayers(event)
 end
 
+function RoundStart(event)
+	PrintWaitingPug(event)
+end
+
+function RoundEnd(event)
+	PrintWaitingPracc(event)
+end
+
+-- Par défaut, on start le warmup
 StartWarmup()
 
+-- Pendant le warmup
 ListenToGameEvent("player_spawn", OnPlayerSpawned, nil)
 
+-- Pendant le lo3
+ListenToGameEvent("round_start", RoundStart, nil)
+
+-- Pendant le pracc
+ListenToGameEvent("round_end", RoundEnd, nil)
+
+-- Permet de voir si le plugin est bien chargé
 print("[LO3] Plugin loaded!")
